c57a0f520c75a3ee0a8150c780596e472303a357
Mahfuz1907 <mahfuztamim1907@gmail.com>
2026-09-24 00:47:56 +0600

    made banner responsive

--- Components/Banner/Banner.tsx
+++ Components/Banner/Banner.tsx
@@ -5,11 +5,11 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className='mx-20 mb-16 p-14 flex flex-row justify-between items-center gap-57 bg-[#15171d] border border-[#222630] rounded-2xl'>
-            <div className='flex flex-col justify-between items-start gap-5'>
+        <div className='mx-5 md:mx-20 mb-16 p-14 flex flex-col-reverse md:flex-row justify-between items-center gap-5 md:gap-57 bg-[#15171d] border border-[#222630] rounded-2xl'>
+            <div className='flex flex-col justify-between items-center md:items-start gap-5'>
                 <h4 className='font-inter text-[#c2f800] text-[11px] font-bold'>WORKOUT LIBRARY</h4>
-                <h1 className='font-oswald text-6xl font-bold text-white'>TRAIN WITH INTENT. LOG EVERY SET.</h1>
-                <p className='text-[#9ca3af] font-inter text-base font-normal'>FitLog is a dark, no-nonsense gym companion: pick a lift, lock it into today&apos;s plan, 
+                <h1 className='font-oswald text-6xl font-bold text-white text-center md:text-start'>TRAIN WITH INTENT. LOG EVERY SET.</h1>
+                <p className='text-[#9ca3af] font-inter text-base font-normal text-center md:text-start'>FitLog is a dark, no-nonsense gym companion: pick a lift, lock it into today&apos;s plan, 
                     and watch the week&apos;s work add up.</p>
                 <button className='browseButton font-inter'>BROWSE WORKOUTS</button>
             </div>
