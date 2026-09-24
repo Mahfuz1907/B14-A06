6db6900c40a54c9ba9a1fca37ca2291947a53927
Mahfuz1907 <mahfuztamim1907@gmail.com>
2026-09-24 12:59:02 +0600

    made library responsive

--- Components/Workouts/Workouts.tsx
+++ Components/Workouts/Workouts.tsx
@@ -7,12 +7,12 @@ const Workouts = async() => {
     const data = await workoutPromise.json()
     
     return (
-        <div className='flex flex-col justify-between items-start gap-3 mx-20 mb-16'>
+        <div className='flex flex-col justify-between items-start gap-3 mx-5 md:mx-20 mb-16'>
             <div className='flex flex-col justify-between items-start gap-1'>
                 <h1 className='text-white font-oswald text-3xl font-bold'>THE LIBRARY</h1>
                 <p className='text-[#9ca3af] font-inter text-sm font-normal'>Twelve lifts covering every major muscle group.</p>
             </div>
-            <div className='grid grid-cols-3 justify-between items-start w-full gap-6'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-between items-start w-full gap-6'>
                 {
                     data.map((work:WorkoutTypesPromises) => <Work key={work.id} work={work} ></Work>)
                 }
