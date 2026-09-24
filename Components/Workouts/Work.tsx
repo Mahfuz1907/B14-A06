28ffa39da0fbb5bbe066c4d683a4cf23b855a6c7
Mahfuz1907 <mahfuztamim1907@gmail.com>
2026-09-24 16:23:43 +0600

    added loading for library

--- Components/Workouts/Work.tsx
+++ Components/Workouts/Work.tsx
@@ -1,25 +1,38 @@
 import { WorkoutTypesPromises } from '@/type';
 import React from 'react';
-import './Work.css'
+import './Work.css';
 import Image from 'next/image';
 import { FaRegClock } from "react-icons/fa";
 import { IoMdFlame } from "react-icons/io";
 import { FaRegStar } from "react-icons/fa";
+import Link from 'next/link';
 
-export interface WorkoutType{
-    work: WorkoutTypesPromises
+export interface WorkoutType {
+  work: WorkoutTypesPromises;
 }
 
-const Work = ({work}: WorkoutType) => {
-    
-    return (
+const Work = ({ work }: WorkoutType) => {
+
+  return (
+    <Link href={`/details/${work.id}`}>
         <div className='workCard'>
-            <Image src={work.image} alt={work.name} width={740} height={300} className='rounded-t-2xl'/>
+            <div className='relative w-full h-48 overflow-hidden rounded-t-2xl'>
+                <Image 
+                src={work.image} 
+                alt={work.name} 
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className='object-cover'
+                />
+            </div>
+
             <div className='flex flex-col justify-between items-start gap-2 p-6'>
                 <div className='flex flex-row justify-start items-center gap-2'>
-                    {
-                        work.muscleGroups.map((muscle)=> <button className='muscleGroupButton font-inter' key={muscle} >{muscle}</button>)
-                    }
+                    {work.muscleGroups.map((muscle) => (
+                        <button className='muscleGroupButton font-inter' key={muscle}>
+                            {muscle}
+                        </button>
+                    ))}
                 </div>
                 <h1 className='text-white font-oswald text-lg font-bold'>{work.name}</h1>
                 <p className='text-[#9ca3af] font-inter text-xs font-normal'>{work.equipment}</p>
@@ -30,7 +43,8 @@ const Work = ({work}: WorkoutType) => {
                 </div>
             </div>
         </div>
-    );
+    </Link>
+  );
 };
 
 export default Work;
---
+++ Components/Workouts/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const workoutLoading = () => {
+    return (
+        <div>
+            <span className="loading loading-spinner text-accent"></span>
+        </div>
+    );
+};
+
+export default workoutLoading;
