c16efc7a98d87c89f974be11d12a756574f46e1f
Mahfuz1907 <mahfuztamim1907@gmail.com>
2026-09-25 15:12:04 +0600

    added sort button

--- Components/Navbar/Navbar.tsx
+++ Components/Navbar/Navbar.tsx
@@ -6,14 +6,14 @@ import Link from 'next/link';
 const Navbar = () => {
     return (
         <div className='sticky top-0 z-50 mb-6 bg-[#090a0d] flex flex-row justify-between items-center px-5 md:px-20 py-4 border border-[#1c1f26]'>
-            <button className='text-white font-oswald text-lg font-bold cursor-pointer'>FITLOG</button>
+            <Link href={'/'} className='text-white font-oswald text-lg font-bold cursor-pointer'>FITLOG</Link>
             <div className='flex flex-row justify-center items-center gap-0 md:gap-2 font-inter'>
                 <Link href={'/'} className='active'>Workouts</Link>
                 <Link href={'/plan'} className='inActive'>My Plan</Link>
             </div>
             <div className='flex flex-row justify-between items-center gap-0 md:gap-2 font-inter'>
-                <button className='planSave planButton'>Plan <span className='planSpan'>0</span></button>
-                <button className='planSave saveButton'>Saved <span className='saveSpan'>0</span></button>
+                <Link href={'/plan'} className='planSave planButton'>Plan <span className='planSpan'>0</span></Link>
+                <Link href={'/plan'} className='planSave saveButton'>Saved <span className='saveSpan'>0</span></Link>
             </div>
         </div>
     );
--- app/plan/Tab.tsx
+++ app/plan/Tab.tsx
@@ -3,9 +3,22 @@ import './Plan.css'
 
 const Tab = () => {
     return (
-        <div role="tablist" className="tabs tabs-box bg-[#151921] border-[#232732] rounded-xl">
-            <a role="tab" className="tab font-inter">Today&apos;s Plan</a>
-            <a role="tab" className="tab tab-active font-inter">Saved</a>
+        <div className='flex flex-row justify-between items-center w-full'>
+            <div role="tablist" className="tabs tabs-box bg-[#151921] border-[#232732] rounded-xl">
+                <a role="tab" className="tab font-inter">Today&apos;s Plan</a>
+                <a role="tab" className="tab tab-active font-inter">Saved</a>
+            </div>
+            <div className='flex flex-row justify-between items-center w-60'>
+                <h1 className='text-[#8a92a0] font-inter font-normal text-xs w-20'>Sort By</h1>
+                <select defaultValue="Pick a color" 
+                className="select bg-[#13161d] border border-[#232732] rounded-[9px] 
+                px-2.5 text-white font-inter text-xs font-normal">
+                    <option disabled={true}>Pick a color</option>
+                    <option>Duration</option>
+                    <option>Calories</option>
+                    <option>Rating</option>
+                </select>
+            </div>
         </div>
     );
 };
