807f54cd693115520070a4e72928844f7d55a132
Mahfuz1907 <mahfuztamim1907@gmail.com>
2026-09-25 15:49:35 +0600

    set navbar active inactive funciton

--- Components/Navbar/Navbar.tsx
+++ Components/Navbar/Navbar.tsx
@@ -1,15 +1,25 @@
+'use client'
+import { usePathname } from 'next/navigation';
+
 import React from 'react';
 import './Navbar.css'
 import Link from 'next/link';
 
 
 const Navbar = () => {
+    const pathname = usePathname()
+
+    const fixStyle = (path:string) => {
+        const isActive = pathname === path
+
+        return isActive ? 'active' : 'inActive'
+    }
     return (
         <div className='sticky top-0 z-50 mb-6 bg-[#090a0d] flex flex-row justify-between items-center px-5 md:px-20 py-4 border border-[#1c1f26]'>
             <Link href={'/'} className='text-white font-oswald text-lg font-bold cursor-pointer'>FITLOG</Link>
             <div className='flex flex-row justify-center items-center gap-0 md:gap-2 font-inter'>
-                <Link href={'/'} className='active'>Workouts</Link>
-                <Link href={'/plan'} className='inActive'>My Plan</Link>
+                <Link href={'/'} className={fixStyle('/')}>Workouts</Link>
+                <Link href={'/plan'} className={fixStyle('/plan')}>My Plan</Link>
             </div>
             <div className='flex flex-row justify-between items-center gap-0 md:gap-2 font-inter'>
                 <Link href={'/plan'} className='planSave planButton'>Plan <span className='planSpan'>0</span></Link>
